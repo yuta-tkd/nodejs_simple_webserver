877e28306e04486ac7ff12e34ee971048605efe5
yuta <ley.ciel@gmail.com>
2015-06-05 17:09:30 +0900

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+//httpモジュール	
+var http = require('http');
+//http.Serverクラスのインスタンス
+var server = http.createServer();
+
+//requestイベントハンドラーの定義
+server.on('request',function(request,response){
+	//リクエストされたURLの表示
+	console.log(request.url);
+	//200(OK)レスポンスと[Content-Type:text/plain]ヘッダーの送信
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	//レスポンス内容
+	response.end('Hello! '+request.url);
+
+});
+
+//8081ポートで待ち受け開始
+server.listen(8081,'localhost');
